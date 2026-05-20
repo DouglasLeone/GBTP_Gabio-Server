a59de828c3dde50d4c6f167fcefbb1f98c920005
clarinhamartins <mariaclaralmeidamartins06@gmail.com>
2026-05-20 17:03:34 -0300

    feat: implementado parcialmente fluxo de comunicaçao

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,30 @@
 import { ComunicadorServer } from "./Comunicacao/comunicadorServer";
+import { GbtpRequestParser } from "./Protocolo/GBTParser";
+import { GbtpResponseBuilder } from "./Protocolo/GBTPResponse";
+import { ProcessAccountUseCase } from "./RegrasNegocio/ProcessAccountUseCase";
+import { AccountRepository } from "./RegrasNegocio/Account";
+import { GbtpRequest } from "./Protocolo/types";
+import { GbtpResponse } from "./Protocolo/types";
 
 const server = new ComunicadorServer();
+const account = new AccountRepository();
+
+const accountUseCase = new ProcessAccountUseCase(account);
+
 
 server.setOnRequest((msg : string) => {
-    
-    console.log("Processando mensagem");
+    try {
+        const parseMsg: GbtpRequest = GbtpRequestParser.parse(msg);
 
-    return `STATUS:OK
-    MESSAGE:Mensagem recebida
-    BALANCE:0`;
+        const processUseCase = accountUseCase.processar(parseMsg)
 
+        const requestMsg = GbtpResponseBuilder.stringifyResponse(processUseCase)
+
+        return requestMsg;
+
+    } catch (error) {
+
+    }
 })
 
 server.startServer(8080);
